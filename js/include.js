93c502e883fcbc5e126390b8cb58988233fb2dbc
zedlu <zed.luigi@gmail.com>
2023-08-11 16:12:26 +0200

    Fixing include.js

--- js/include.js
+++ js/include.js
@@ -1,7 +1,7 @@
-$(function  loader() {
+function  loader() {
     var includes = $('[data-include]')
     $.each(includes, function () {
         var file = 'GiacchettiLuigi-Portfolio/html/' + $(this).data('include') + '.html';
         $(this).load(file)
     })
-    })
+}
